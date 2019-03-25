309408b9302285299c5baae0d56a6192c91bf2a2
sillanithin <48715193+sillanithin@users.noreply.github.com>
2019-03-25 18:48:14 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,13 @@
+try:
+    n=int(input())
+    if(n>0):
+        if n%2==0:
+            print("Even")
+        elif n%2!=0:
+            print("Odd")
+        else:
+            print("invalid")
+    else:
+        print("invalid")
+except:
+    print("invalid")
