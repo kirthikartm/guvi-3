f3037a27df7acbbaf1deb94134f408f0548e0d27
sillanithin <48715193+sillanithin@users.noreply.github.com>
2019-03-25 20:59:40 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,13 @@
+try:
+    n=int(input())
+    if n%100==0:
+        if n%400==0:
+            print("yes")
+        else:
+            print("no")
+    elif n%4==0:
+        print("yes")
+    else:
+        print("no")
+except:
+    print("invalid")
