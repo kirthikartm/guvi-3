2bbf7710b862ff0d097128f6f4ded61a2b76e802
sillanithin <48715193+sillanithin@users.noreply.github.com>
2019-03-25 19:55:33 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,9 @@
+try:
+    n=input()
+    v=['a','e','i','o','u','A','E','I','O','U','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','w','x','y','z','Z','Y','X','W','T','S','R','Q','P','N','M','L','K','J','H','G','F','D','C','B']
+    if n in v:
+        print("Alphabet")
+    else:
+    	print("No")
+except:
+    print("invalid")
