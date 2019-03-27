ac25f85b881ff73c3c5128c412ee4f560b4920fe
sillanithin <48715193+sillanithin@users.noreply.github.com>
2019-03-27 19:16:26 +0530

    Create ieven.py

---
+++ ieven.py
@@ -0,0 +1,18 @@
+try:
+    n=input()
+    n=n.split()
+    l=[]
+    for i in n:
+        t=int(i)
+        l.append(t)
+    a=l[0]
+    b=l[1]
+    k=[]
+    for j in range(a+1,b):
+        if(j%2==0):
+            k.append(j)
+    for i in range(len(k)-1):
+        print(k[i],end=' ')
+    print(k[-1])
+except:
+    print("invalid")
